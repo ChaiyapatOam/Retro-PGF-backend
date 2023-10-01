56682b654ea409074999f1d48e180cc29015a9e7
ChaiyapatOam <chaiyapatoam12@gmail.com>
2023-10-02 02:18:41 +0700

    chore: set cors retropdf

--- src/server.ts
+++ src/server.ts
@@ -11,7 +11,7 @@ const app: Express = express();
 const port = process.env.PORT || 8080;
 
 app.use(helmet());
-app.use(cors({ origin: "http://127.0.0.1:5173", credentials: true }));
+app.use(cors({ origin: "https://www.retropgfhub.com", credentials: true }));
 // app.use(cors({ origin: "", credentials: true }));
 
 app.use(express.json());
